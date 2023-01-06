96fe4c3488ccbf0740d31b910d8f470f0d314c94
Jeremy Fisher <jeremyadamsfisher@gmail.com>
2023-01-05 22:44:26 -0800

    Add submodule

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="podcast2podcast",
     version="0.1.0",
-    packages=find_packages(include=["podcast2podcast"]),
+    packages=find_packages(include=["podcast2podcast", "podcast2podcast.*"]),
     install_requires=[
         "toml",
         "dynaconf",
