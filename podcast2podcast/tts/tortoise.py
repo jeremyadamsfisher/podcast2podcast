e8e9e7c1e21e4f0eb550d9ac2574ca5d379468e9
Jeremy Fisher <jeremyadamsfisher@gmail.com>
2023-01-09 17:58:04 -0800

    Run tortoise on each sentence to avoid overly long runs

--- podcast2podcast/tts/tortoise.py
+++ podcast2podcast/tts/tortoise.py
@@ -1,25 +1,10 @@
 from tempfile import NamedTemporaryFile
-from typing import Literal, Tuple
+from typing import Literal
 
 import torchaudio
-from loguru import logger
 from pydub import AudioSegment
 
 from podcast2podcast.nlp import nlp
-
-
-def split_transcript(transcript: str) -> Tuple[str, str]:
-    """Split a transcript into two parts.
-
-    Args:
-        transcript (str): Transcript.
-
-    Returns:
-        Tuple[str, str]: First and second half of transcript.
-    """
-    doc = nlp(transcript)
-    sents = [s.text.strip() for s in doc.sents]
-    return " ".join(sents[: len(sents) // 2]), " ".join(sents[len(sents) // 2 :])
 
 
 def text2speech_pipeline(
@@ -41,23 +26,24 @@ def text2speech_pipeline(
 
     tts = TextToSpeech()
     mouse_voice_samples, mouse_conditioning_latents = load_voice("train_mouse")
-    try:
-        speech = tts.tts_with_preset(
-            transcript,
-            preset=preset,
-            voice_samples=mouse_voice_samples,
-            conditioning_latents=mouse_conditioning_latents,
-        )
-    except AssertionError:
-        logger.warning(
-            "Tortoise cannot deal with very long texts. Rerunning tortoise on "
-            "smaller segments of the text and concatenating the results."
-        )
-        first_half, second_half = split_transcript(transcript)
-        return text2speech_pipeline(first_half) + text2speech_pipeline(second_half)
 
-    with NamedTemporaryFile(suffix=".wav") as t:
-        torchaudio.save(t.name, speech.squeeze(0).cpu(), 24000)
-        audio = AudioSegment.from_wav(t.name)
+    segments = []
+    for sentence in nlp(transcript).sents:
+        try:
+            speech = tts.tts_with_preset(
+                sentence.strip(),
+                preset=preset,
+                voice_samples=mouse_voice_samples,
+                conditioning_latents=mouse_conditioning_latents,
+            )
+        except AssertionError:
+            raise ValueError("Tortoise cannot deal with long texts.")
+        else:
+            with NamedTemporaryFile(suffix=".wav") as t:
+                torchaudio.save(t.name, speech.squeeze(0).cpu(), 24000)
+                segment = AudioSegment.from_wav(t.name)
+                segments.append(segment)
+
+    audio = sum(segments)
 
     return audio
