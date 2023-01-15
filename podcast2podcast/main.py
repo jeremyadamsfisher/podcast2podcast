3085ccc9e5547bb28cccc4958cbd524ed6a5c28e
Jeremy Fisher <jeremyadamsfisher@gmail.com>
2023-01-14 19:41:33 -0800

    Remove pydub import

--- podcast2podcast/main.py
+++ podcast2podcast/main.py
@@ -1,9 +1,6 @@
 import io
 from contextlib import redirect_stdout
-from typing import Literal
-
-from pydub import AudioSegment
-from unidecode import unidecode
+from typing import TYPE_CHECKING, Literal
 
 from podcast2podcast.dialog import new_dialog
 from podcast2podcast.rss import get_podcast_details
@@ -11,12 +8,15 @@ from podcast2podcast.tts.google import tts as google_tts
 from podcast2podcast.tts.tortoise import tts as tortoise_tts
 from podcast2podcast.utils import yap
 
+if TYPE_CHECKING:
+    from pydub import AudioSegment
+
 
 def pipeline(
     url,
     episode_idx,
     tts_method: Literal["google", "tortoise"] = "google",
-) -> AudioSegment:
+) -> "AudioSegment":
     """Run the entire pipeline (transcription to spoken output).
 
     Args:
@@ -28,7 +28,6 @@ def pipeline(
         AudioSegment: Audio of podcast episode.
 
     """
-
     buffer = io.StringIO()
     with redirect_stdout(buffer):
         with yap(about="getting podcast information"):
@@ -38,5 +37,4 @@ def pipeline(
         with yap(about="generating audio"):
             tts = {"google": google_tts, "tortoise": tortoise_tts}[tts_method]
             audio = tts(transcript)
-
     return audio
